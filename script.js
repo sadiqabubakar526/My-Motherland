0e3bb11348ec30f44df38d3eb827fe359e0100d6
sadiqabubakar526 <surajabubakar443@gmail.com>
2021-02-01 03:17:18 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+
+    alert("The Nigeria National Flag: \nIt remains beautiful and proud, always- be it day or night!\n\nI am proud to be a Nigerian and every true patriot is proud to be an inhabitant of his/her own country, isn't it?\n\nThis is one of my attempts to pay respect to my motherland; 🇳🇬");
+ 
